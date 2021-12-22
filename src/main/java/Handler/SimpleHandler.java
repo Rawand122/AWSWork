5fe7e672c56ae5e4a60824ee9ec7ac26d9bee2a1
Rawand122 <rawandhassan@hotmail.com>
2021-12-22 02:14:20 +0000

    added - retreving s3 file/ using env for credientials/ printing bytes

--- src/main/java/Handler/SimpleHandler.java
+++ src/main/java/Handler/SimpleHandler.java
@@ -1,12 +1,25 @@
 package Handler;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+import java.util.Arrays;
 
 public class SimpleHandler implements RequestHandler<S3Event, Object> {
-
+    public static final String accessKey = System.getenv("accessKey");
+    public static final String privateKey =System.getenv("privateKey");
 
     @Override
     public String handleRequest(S3Event input, Context context) {
@@ -15,9 +28,29 @@ public class SimpleHandler implements RequestHandler<S3Event, Object> {
         for(S3EventNotification.S3EventNotificationRecord record : input.getRecords()){
             String fileName = record.getS3().getObject().getKey();
             String bucketName = record.getS3().getBucket().getName();
-
             System.out.println(fileName + "Bucket: " + bucketName);
 
+            // now to download  file
+            Regions clientRegion = Regions.EU_WEST_2;
+
+            S3Object fullObject = null, objectPortion = null, headerOverrideObject = null;
+            try {
+
+                AWSCredentials credentials = new BasicAWSCredentials(accessKey,privateKey);
+                AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                        .withRegion(clientRegion)
+                        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                        .build();
+
+                // Get an object and print its contents.
+                System.out.println("Downloading an object");
+                fullObject = s3Client.getObject(new GetObjectRequest(bucketName,fileName));
+                System.out.println("Content-Type: " + fullObject.getObjectMetadata().getContentType());
+                System.out.println("Content: ");
+                System.out.println(Arrays.toString(fullObject.getObjectContent().readAllBytes()));
+            } catch(Exception e){
+
+            }
 
         }
 
---
+++ src/main/java/Processor/S3FileReader.java
@@ -0,0 +1,12 @@
+package Processor;
+
+public class S3FileReader {
+
+    //Class to take filename / bucket name and then download file from s3 and print values
+
+
+    public static void CSVS3Reader(String fileName, String bucketName){
+
+
+    }
+}
